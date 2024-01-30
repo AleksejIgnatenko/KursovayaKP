29928efb4387cb5e4ce26783845f71b5ee1a43d3
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-01-30 14:43:11 +0300

    Созданны формы регистрации и входа, модель "User", валидация модели "User" и стили.

---
+++ KursovayaKP/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using KursovayaKP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace KursovayaKP.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Sign_In()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check_Sign_In(UserModel userModel)
+        {
+/*            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }*/
+            return View("Sign_In");
+        }
+
+        [HttpPost]
+        public IActionResult Check_registration(UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Обработка успешной валидации
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Валидация не удалась, возвращаем модель с ошибками
+            return View("Registration", userModel);
+        }
+    }
+}
---
+++ KursovayaKP/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KursovayaKP.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Неверный логин")]
+        [MinLength(3, ErrorMessage = "Имя не должно быть меньше 3 символов")]
+        public string UserName { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Неверный Email")]
+        public string Email { get; set; }
+
+        [MinLength(8, ErrorMessage = "Пароль должен состоять минимум из 8 символов")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string Repeat_password { get; set; }
+
+        public string Role { get; set; } = "User";
+    }
+}
