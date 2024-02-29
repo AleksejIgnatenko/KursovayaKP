dc6aa3f28d9aff1735d1be3769cdd1620e808eed
Aleksej Ignatenko <ignatenkoaleksej771@gmail.com>
2024-02-29 09:43:02 +0300

    Добавлен прототип меню с инструментами в AllQuestions.cshtml.

---
+++ KursovayaKP/wwwroot/js/AllQuestions.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("settingsButton").addEventListener("click", function () {
+        var toolsMenu = document.getElementById("toolsMenu");
+        if (toolsMenu.style.display === "none") {
+            toolsMenu.style.display = "block";
+        } else {
+            toolsMenu.style.display = "none";
+        }
+    });
+});
